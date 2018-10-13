dce0826782c2051ef5ce4efb7713b014ad3f4c50
songya <songya@umich.edu>
2018-10-13 08:08:26 -0400

    imports in index.js files

---
+++ bingeworthy/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { user } from './user';
+//import { navReducer } from './navReducer';
+
+export default combineReducers({
+  //nav: navReducer,
+  //     user
+});
