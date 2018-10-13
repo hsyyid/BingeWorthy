5ab4cf8248098f504668872875ac3939008d5c26
songya <songya@umich.edu>
2018-10-13 09:45:17 -0400

    aws

--- bingeworthy/src/App.js
+++ bingeworthy/src/App.js
@@ -3,12 +3,17 @@ import React from 'react';
 //only works with ios11 -
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import Amplify, { withAuthenticator } from 'aws-amplify-react-native';
 import reducers from './reducers';
+import aws_exports from './aws-exports';
 
 import { Page } from './components';
+
+Amplify.configure(aws_exports);
+
 //<Provider store={createStore(reducers)}>
 const App = () => (
     <Page />
 );
 //</Provider>
-export default App;
+export default withAuthenticator(App);
