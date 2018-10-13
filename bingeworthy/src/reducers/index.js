5daa7f04a4c32c6349ce3d275727e067852dc02b
songya <songya@umich.edu>
2018-10-13 10:10:51 -0400

    imported pageReducer wrong

--- bingeworthy/src/reducers/index.js
+++ bingeworthy/src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import { user } from './user';
-import { pageReducer } from './pageReducer';
+import pageReducer from './pageReducer';
 //import { navReducer } from './navReducer';
 
-export default combineReducers({
-  selectedPage: pageReducer
-});
+export default combineReducers({ selectedPage: pageReducer });
