840e9c0ab06e8444d87aafd032bd5080fde6e94d
Hassan Syyid <hassansyyid@gmail.com>
2018-10-13 21:36:26 -0400

    Delete Firebase Cloudstore + Add LogOut Function

--- bingeworthy/src/reducers/user.js
+++ bingeworthy/src/reducers/user.js
@@ -1,14 +1,4 @@
 import {Auth} from 'aws-amplify';
-
-// const admin = require('firebase-admin');
-// const serviceAccount = require('../firebaseAccountKey.json');
-//
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// });
-//
-// const db = admin.firestore();
-
 import fetch from 'cross-fetch';
 
 const endpoint = "https://fty0veyci2.execute-api.us-east-2.amazonaws.com/latest";
@@ -17,41 +7,9 @@ export function getUser() {
     return Auth.currentAuthenticatedUser();
 }
 
-// function getUserRef(username) {
-//     return new Promise((resolve) => {
-//         getUser().then(user => {
-//             console.log(JSON.stringify(user, null, 2));
-//
-//             // Get the user ref
-//             resolve(db.collection('users').doc(username));
-//         });
-//     });
-// }
-//
-// export function setUserStatus(online) {
-//     getUserRef().then(ref => {
-//         ref.update({online});
-//     });
-// }
-//
-// /**
-//  * Registers a listener for each friend to listen to
-//  */
-// export function listenToFriendsStatus() {
-//     // TODO: Get friends
-//     let friends = [];
-//
-//     friends.forEach(f => {
-//         getUserRef(f.username).then(doc => {
-//             doc.onSnapshot(docSnapshot => {
-//                 console.log(`Received doc snapshot: ${docSnapshot}`);
-//                 // TODO: Depending on what this snapshot is, update our 'onlineFriends' in state...
-//             }, err => {
-//                 console.log(`Encountered error: ${err}`);
-//             });
-//         });
-//     });
-// }
+export function logOut() {
+   return Auth.signOut();
+}
 
 export function connectSpotify(code) {
     getUser().then(user => {
