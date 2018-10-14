4c6e9fd14aba0b8a9cb523351cf99dfff937f062
songya <songya@umich.edu>
2018-10-13 20:24:44 -0400

    style object for changing the size of labelstyles

--- bingeworthy/src/components/BottomNavigator.js
+++ bingeworthy/src/components/BottomNavigator.js
@@ -1,12 +1,32 @@
 import { createMaterialBottomTabNavigator } from
       'react-navigation-material-bottom-tabs';
+import React from 'react';
+import { Text } from 'react-native';
+
 import Feed from './Feed';
 
+const labelStyle = {
+  fontSize: 17
+};
+
 export default createMaterialBottomTabNavigator({
-  Profile: { screen: Feed },
-  Feed: { screen: Feed },
-  Post: { screen: Feed },
-}, {
+  Profile: {
+    screen: Feed,
+    navigationOptions: {
+      tabBarLabel: <Text style={labelStyle}>Profile</Text>
+      }
+    },
+  Feed: { screen: Feed,
+    navigationOptions: {
+      tabBarLabel: <Text style={labelStyle}>Feed</Text>
+    }
+  },
+  Post: { screen: Feed,
+    navigationOptions: {
+      tabBarLabel: <Text style={labelStyle}>Post</Text>
+    }
+  }
+  }, {
   initialRouteName: 'Feed',
   activeColor: '#f0edf6',
   inactiveColor: '#3e2465',
